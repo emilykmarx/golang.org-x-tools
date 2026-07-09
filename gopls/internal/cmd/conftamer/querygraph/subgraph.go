8f5570f24d29af7e8124add8aeea31ce99062766
Emily Marx <emilykmarx@alumni.stanford.edu>
2026-07-08 17:58:07 -0700

    querygraph: Also support querying for shortest path.

--- gopls/internal/cmd/conftamer/querygraph/subgraph.go
+++ gopls/internal/cmd/conftamer/querygraph/subgraph.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
 	"log/slog"
 	"os"
@@ -11,24 +12,34 @@ import (
 	ct "golang.org/x/tools/gopls/internal/cmd/conftamer"
 )
 
-// Get all paths containing the type_name.
+// Get all paths containing the start_type_name, or shortest path from start_type_name to end_type_name.
 // Serialize and write to .gv the resulting subgraph.
 func main() {
-	var infile, outfile, type_name string
+	var infile, outfile, start_type_name, end_type_name string
 	flag.StringVar(&infile, "infile", "", "File containing serialized full graph")
 	flag.StringVar(&outfile, "outfile", "", "Filename prefix for subgraphs (serialized will be <outfile>.txt, graphviz will be <outfile>.gv)")
-	flag.StringVar(&type_name, "type", "", "Query type")
+	flag.StringVar(&start_type_name, "start-type", "", "Query start type (mandatory)")
+	flag.StringVar(&end_type_name, "end-type", "", "Query end type (optional - "+
+		"if passed, will find shortest path from start to end, rather than all paths containing start)")
 	flag.Parse()
-	if infile == "" || outfile == "" || type_name == "" {
+	if infile == "" || outfile == "" || start_type_name == "" {
 		flag.Usage()
 		log.Fatalf("Missing mandatory argument")
 	}
 
 	g, m := ct.Deserialize(infile)
 	full_g := ct.CTypes{Graph: g, List: m.List}
-	hash, ok := full_g.GetHash(ct.FullTypeName(type_name))
+
+	start_hash, ok := full_g.GetHash(ct.FullTypeName(start_type_name))
 	if !ok {
-		panic("type not found")
+		panic(fmt.Errorf("type %v not found", start_type_name))
+	}
+	end_hash := ct.CTypeHash("")
+	if end_type_name != "" {
+		end_hash, ok = full_g.GetHash(ct.FullTypeName(end_type_name))
+		if !ok {
+			panic(fmt.Errorf("type %v not found", end_type_name))
+		}
 	}
 
 	// needed for logging in graph lib
@@ -44,8 +55,14 @@ func main() {
 	g.SetLog(log)
 
 	start := time.Now()
-	graph.Logf(g.Log(), slog.LevelInfo, "Querying for paths containing %v", type_name)
-	sub_g := graph.Query(g, hash)
+	var end *ct.CTypeHash
+	if end_type_name != "" {
+		end = &end_hash
+		graph.Logf(g.Log(), slog.LevelInfo, "Querying for path %v => %v", start_type_name, end_type_name)
+	} else {
+		graph.Logf(g.Log(), slog.LevelInfo, "Querying for paths containing %v", start_type_name)
+	}
+	sub_g := graph.Query(g, start_hash, end)
 	graph.Logf(g.Log(), slog.LevelInfo, "Query time: %v", time.Since(start))
 
 	// reuse list (will be superset of nodes actually in subgraph)
