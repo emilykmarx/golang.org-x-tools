8101af3d1eb1b0c951ac130c99e8e4b382d0cff0
Emily Marx <emilykmarx@alumni.stanford.edu>
2026-09-21 18:22:06 -0700

    stacks: Make graph of function calls in ancestry, leading to messages
    identified by destination port.
    
    i.e. each function is a node, with an edge from the previous function
    (either in own stack or parent's), and an edge to a message node if it's
    the sending frame.
    
    Remove nodes previously created for types, since currently unsure how to make that a useful graph.
    
    Looks pretty good on contextblog, but is a bit deceiving since if two
    different paths converge at some function but lead to two different
    messages, it looks like both paths lead to both messages (e.g. in
    contextblog, if we were to have a node for conn.Write).

--- gopls/internal/cmd/conftamer/stacks/parse.go
+++ gopls/internal/cmd/conftamer/stacks/parse.go
@@ -3,11 +3,14 @@ package parse
 import (
 	"bufio"
 	"context"
+	"errors"
 	"fmt"
+	"net"
 	"os"
 	"path/filepath"
 	"strings"
 
+	graph "github.com/emilykmarx/dominikbraun-graph"
 	ct "golang.org/x/tools/gopls/internal/cmd/conftamer"
 	"golang.org/x/tools/gopls/internal/protocol"
 	"golang.org/x/tools/gopls/internal/server"
@@ -51,21 +54,66 @@ func DeanonymizeFn(fn *string) {
 	*fn, _, _ = strings.Cut(*fn, ".func")
 }
 
+func CleanConnPart(part string) string {
+	part = strings.Trim(part, "{}")
+	return part[strings.Index(part, ":")+1:]
+}
+
+// would be easier if net marshaled this, but that's annoying too
+// return net, laddr, raddr
+func ParseConn(conn string) []string {
+	parts := strings.Split(conn, " ")
+	for i, part := range parts {
+		parts[i] = CleanConnPart(part)
+	}
+	return parts
+}
+
+func ParseDstPort(conn string) string {
+	parsed_conn := ParseConn(conn)
+	_, dst_port, err := net.SplitHostPort(parsed_conn[len(parsed_conn)-1])
+	ct.CheckErr(err)
+	return dst_port
+}
+
+func AddSentMsgNode(conn string, sending_types *ct.CTypes) ct.CTypeHash {
+	dst_port := ParseDstPort(conn)
+	hash := "DPORT: " + dst_port
+	new_ctype := ct.CTypeNode{Names: []ct.FullTypeName{ct.FullTypeName(hash)}}
+	_ = sending_types.Graph.AddVertex(new_ctype, func(vp *graph.VertexProperties) {}) // ok if existed
+	return ct.CTypeNodeHash(new_ctype)
+}
+
+// return hash and whether existed
+func AddSendFuncNode(fn string, sending_types *ct.CTypes) (ct.CTypeHash, bool) {
+	hash := fn
+	new_ctype := ct.CTypeNode{Names: []ct.FullTypeName{ct.FullTypeName(hash)}}
+	existed := sending_types.Graph.AddVertex(new_ctype, func(vp *graph.VertexProperties) {}) // ok if existed
+	if existed != nil {
+		if !errors.Is(existed, graph.ErrVertexAlreadyExists) {
+			ct.CheckErr(existed)
+		}
+	}
+	return ct.CTypeNodeHash(new_ctype), existed != nil
+}
+
 // Parse the stacks of the given conn,
 // writing any frames that failed to parse to err_file
 func parseConnStacks(conn string, stacks []string, sending_types *ct.CTypes,
-	server *server.Server, err_file *os.File, seen_fns map[string]struct{}) {
+	server *server.Server, err_file *os.File) {
 
-	// LEFT OFF what should we do with the sending types? Some way to ID the msg would be useful, e.g. what packages involved in send (i.e. packages of fns in stack)
-	// May want to ignore udp or treat separately (will likely see lots of DNS)
 	sending_g := 0
 	_, err := fmt.Sscanf(stacks[0], "goroutine %d", &sending_g)
 	ct.CheckErr(err)
-	fmt.Printf("%+v\n", conn)
+	fmt.Printf("%+v\n", ParseConn(conn))
 	fmt.Printf("SENDING G %v\n", sending_g)
+
+	// Make a node for the message, identified by destination port
+	msg_hash := AddSentMsgNode(conn, sending_types)
 
 	g_header := "[originating from goroutine "
 	g := 0
+	prev_fn := msg_hash
 	for _, line := range stacks {
 		if strings.Contains(line, g_header) {
 			_, err := fmt.Sscanf(line, g_header+"%d", &g)
@@ -78,32 +126,41 @@ func parseConnStacks(conn string, stacks []string, sending_types *ct.CTypes,
 			// Assume the last ( is the beginning of the args
 			fn := line[:strings.LastIndex(line, "(")]
 			SanitizeMethod(&fn)
-			if fn == "main.main" || fn == "net.conn.log" {
+			if fn == "main.main" || fn == "net.conn.log" || fn == "net.conn.Write" {
 				// Ignore logging function
 				continue
 			}
 			fmt.Printf("FN: %v\n", fn)
-			if _, seen := seen_fns[fn]; !seen {
-				seen_fns[fn] = struct{}{}
+			cur_fn, existed := AddSendFuncNode(fn, sending_types)
+			if prev_fn != cur_fn { // don't add self-edges
+				// add edge to previous frame (in parent g's stack if applicable),
+				// or to sent message (if this is the sending frame)
+				err := sending_types.Graph.AddEdge(cur_fn, prev_fn, graph.EdgeWeight(1))
+				if err != nil {
+					if !errors.Is(err, graph.ErrEdgeAlreadyExists) {
+						ct.CheckErr(err)
+					}
+				}
+				prev_fn = cur_fn
 
 				p := protocol.WorkspaceSymbolParams{
 					Query: fn,
 				}
-				arg_types, err := server.ArgTypes(context.Background(), &p)
-				if err != nil {
-					// Write failure to log
-					err_log := []string{fn, err.Error()}
-					_, err := err_file.WriteString(strings.Join(err_log, "\n") + "\n")
-					ct.CheckErr(err)
-					continue
-				}
-				// If slow, can skip gopls calls for fns we've already made (e.g. conn.Write)
-				fmt.Printf("ARGS:\n")
-				for _, arg := range arg_types {
-					if !ct.BasicType(arg) {
-						fmt.Printf("%v\n", arg.TypeInfo.Name())
-						_, err := sending_types.AddCType(arg, nil)
+				if !existed { // avoid gopls call if already did it for this fn
+					arg_types, err := server.ArgTypes(context.Background(), &p)
+					if err != nil {
+						// Write failure to log
+						// Would be useful to print next line here too, and line as is w/o sanitize (so can search logs easier)
+						err_log := []string{fn, err.Error()}
+						_, err := err_file.WriteString(strings.Join(err_log, "\n") + "\n")
 						ct.CheckErr(err)
+						continue
+					}
+					fmt.Printf("ARGS:\n")
+					for _, arg := range arg_types {
+						if !ct.BasicType(arg) {
+							fmt.Printf("%v\n", arg.TypeInfo.Name())
+						}
 					}
 				}
 			}
@@ -113,6 +170,7 @@ func parseConnStacks(conn string, stacks []string, sending_types *ct.CTypes,
 
 // Parse a log of stacktraces (including ancestor goroutines) with a specific format.
 // Write parse failures to a log.
+// Run from the directory containing module source code, since gopls will analyze it
 func ParseStacksLog(sending_types *ct.CTypes, send_log string, output_path string, local_server *server.Server) {
 	send_file, err := os.Open(send_log)
 	ct.CheckErr(err)
@@ -122,11 +180,9 @@ func ParseStacksLog(sending_types *ct.CTypes, send_log string, output_path strin
 	ct.CheckErr(err)
 	defer err_file.Close()
 
-	seen_fns := make(map[string]struct{}) // avoid querying gopls multiple times for a function
-
 	scanner := bufio.NewScanner(send_file)
 	for conn, stacks := parseOneConn(scanner); conn != ""; conn, stacks = parseOneConn(scanner) {
-		parseConnStacks(conn, stacks, sending_types, local_server, err_file, seen_fns)
+		parseConnStacks(conn, stacks, sending_types, local_server, err_file)
 	}
 
 	if err := scanner.Err(); err != nil {
